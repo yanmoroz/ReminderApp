c10bcf5afd61c5c59ebc35ae0b553bcae27a3e49
Yan Moroz <mail@yanmoroz.ru>
2024-02-13 01:13:10 +0300

    Initial Commit

---
+++ ReminderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReminderApp
+//
+//  Created by Yan Moroz on 13.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
